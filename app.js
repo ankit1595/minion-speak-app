4ba5f9d5d729a5c8f9c6b9a72e0e4fb975cfd174
Ankit <ankit.95sharma@gmail.com>
2022-01-17 16:53:01 +0530

    button, textarea & output
    learned querySelector(), addEventListener(); read data from textarea & div also write to div-output box

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-console.log("Script successfully running from other file");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
 
-// prompt("Hi! Can I ask you a question?");
-// alert("MY name is ankit. if you are going to ask this..");
+function clickEventHandler() {
+    console.log("clicked");
+    console.log("Text inside text area - ", txtInput.value);
+    txtOutput.innerText="Text will be copied from input " + txtInput.value;
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
