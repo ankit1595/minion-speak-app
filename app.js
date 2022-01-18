2e390f9ffcfa99fb077218f78e0359a905c24513
Ankit <ankit.95sharma@gmail.com>
2022-01-18 17:30:43 +0530

    added api calls

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 
+serverURL = "https://api.funtranslations.com/translate/?text=";
+
+function getTranslationURL(text) {
+    var textURL=serverURL+text;
+    return textURL;
+}
+function errorHandler(){
+    console.log("Error Occured",error);
+    alert("Something wrong with server! please try again later!!!")
+}
 function clickEventHandler() {
-    console.log("clicked");
-    console.log("Text inside text area - ", txtInput.value);
-    txtOutput.innerText="Text will be copied from input " + txtInput.value;
+    var URL=getTranslationURL(txtInput.value);
+    fetch(URL)
+    .then(response => response.json())
+    .then(json => txtOutput.innerText=json.contents.text)
+    .catch(errorHandler)
+    ;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
